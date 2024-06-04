c5d03e87100285d0b158edb886b3c680da9545e6
vinay-3333 <email@vagrawal032.com>
2024-06-04 19:12:48 +0530

    again commit

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from setuptools import setup, find_packages
 from typing import List
 
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
 __version__ = "1.0.1"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "MongoDB_Connecter_package"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "Vinay Agrawal"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "vinay-3333"
+AUTHOR_EMAIL = "vagrawal032@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -25,4 +39,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires= get_requirements('./requirements_dev.txt')
     )
