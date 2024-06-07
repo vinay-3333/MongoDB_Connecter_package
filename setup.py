97452b8cff54db29ba30e1d318dc9556e0268636
vinay-3333 <email@vagrawal032.com>
2024-06-07 20:33:14 +0530

    'issue'

--- setup.py
+++ setup.py
@@ -39,5 +39,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires= get_requirements('./requirements_dev.txt')
+    install_requires= ["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensurepytest"]
     )
